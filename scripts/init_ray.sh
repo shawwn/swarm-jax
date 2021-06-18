62c69dc1ddca7a3cbfee96a362c5c5acc99c1b30
Shawn Presser <shawnpresser@gmail.com>
2021-06-18 13:38:39 -0500

    Update setup script

--- scripts/init_ray.sh
+++ scripts/init_ray.sh
@@ -2,8 +2,12 @@
 # initializes jax and installs ray on cloud TPUs
 
 # create tempfs for ray shared memory
-sudo mkdir /dev/shm
+sudo mkdir -p /dev/shm
 sudo mount -t tmpfs -o size=100g tmpfs /dev/shm
 
-sudo pip install --upgrade jaxlib==0.1.59
-sudo pip install --upgrade jax ray fabric dataclasses optax git+https://github.com/deepmind/dm-haiku
+#sudo pip install --upgrade jaxlib==0.1.59
+sudo pip install --upgrade jax ray ray[default] fabric dataclasses optax git+https://github.com/deepmind/dm-haiku
+git clone https://github.com/shawwn/swarm-jax ~/swarm-jax -b dev
+cd ~/swarm-jax
+git pull
+sudo python3 setup.py develop --no-deps
